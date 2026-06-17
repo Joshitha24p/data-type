791fc76ad6efeb9e78643525f23d2560c2e4bb63
Joshitha24p <joshitha.joshi2012@gmail.com>
2026-06-17 10:31:00 -0400

    Add files via upload

---
+++ type cast.py
@@ -0,0 +1,13 @@
+detail="Joshitha"
+age=14
+is_student=True
+weight=44.5
+age=str(age)
+print (age)
+print ("type of age:", type(age))
+weight=int(weight)
+print (weight)
+print ("type of weight:", type(weight))
+is_student=str(is_student)
+print (is_student)
+print ("type of is_student:", type (is_student))
