907b1ebe861c69b3b6ee1e868bc0d9557f842b05
Joshitha24p <joshitha.joshi2012@gmail.com>
2026-06-17 10:22:49 -0400

    Add files via upload

---
+++ data types.py
@@ -0,0 +1,12 @@
+detail="Joshitha"
+age=14
+is_student=True
+weight=44.5
+print ("detail:", detail)
+print ("type of detail:", type(detail))
+print ("age:", age)
+print ("type of detail:", type(age))
+print ("is_student:", is_student)
+print ("type of is_student:", type(is_student))
+print ("weight:", weight)
+print ("type of weight:", type(weight))
